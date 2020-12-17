39c0d2a17851b63c15158050a8e344c8c0de4ce9
eze8789 <ezequiel.reyna@gmail.com>
2020-12-17 01:44:26 -0300

    Move postgres db and table initialization completely outside of main

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/eze8789/urlshtn-go/pkg/config"
 	"github.com/eze8789/urlshtn-go/pkg/database/postgres"
 	"github.com/sirupsen/logrus"
@@ -15,18 +14,10 @@ func main() {
 	if err != nil {
 		logrus.Fatalf("could not read configuration file %s: %v", *configFile, err)
 	}
-	// TODO Remove print statements
-	fmt.Println(cfg.Postgres.Port)
-	pgConn, err := postgres.NewConn(cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.Username, cfg.Postgres.Password,
+	pg, err := postgres.NewConn(cfg.Postgres.Host, cfg.Postgres.Port, cfg.Postgres.Username, cfg.Postgres.Password,
 		cfg.Postgres.Database, cfg.Postgres.SSLMode)
 	if err != nil {
 		logrus.Fatalf("could not establish connection: %v", err)
 	}
-	defer pgConn.DB.Close()
-
-	err = pgConn.CreateTable("./configs/sql/create_url_shortener.sql")
-	if err != nil {
-		logrus.Fatalf("could not create table: %v", err)
-	}
-
+	defer pg.DB.Close()
 }
